638e3ef940452ce50e6f88a698779d02f49beb03
VitorH3 <vitoraandradeh3@gmail.com>
2026-04-15 20:50:10 -0300

    refactor: change dropzone default mode

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -9,10 +9,12 @@ import { usePendingUploads } from "../store/uploads";
 export function UploadWidget() {
    const { isThereAnyPendingUploads } = usePendingUploads();
 
-   const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true);
+   // const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true);
+   const [isWidgetOpen, toggleWidgetOpen] = useCycle(true, false);
 
    return (
-      <Collapsible.Root onOpenChange={() => toggleWidgetOpen()} asChild>
+      // <Collapsible.Root onOpenChange={() => toggleWidgetOpen()} asChild>
+      <Collapsible.Root defaultOpen onOpenChange={() => toggleWidgetOpen()} asChild>
          <motion.div
             data-progress={isThereAnyPendingUploads}
             className="bg-zinc-900 max-w-[360px] overflow-hidden rounded-xl shadow-2xl border border-transparent animate-border data-[state=closed]:rounded-3xl data-[state=closed]:data-[progress=false]:shadow-shape data-[state=closed]:data-[progress=true]:[background:linear-gradient(to_bottom,rgba(95, 2, 245, 0.02),rgba(118, 3, 250, 0.01))]"
